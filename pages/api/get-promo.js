9e0d5fa12e4851c20f5b4074a6bde773c298b6ea
Wendel Brito de Oliveira <wendelbrito23@gmail.com>
2020-07-23 10:07:01 -0300

    adicionando base64

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -1,13 +1,14 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
-
+import { fromBase64 } from '../../utils/base64'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
-export default async(req, res) => {
+
+export default async (req, res) => {
     try{
         await doc.useServiceAccountAuth({
             client_email: process.env.SHEET_CLIENT_EMAIL,
-            private_key: process.env.SHEET_PRIVATE_KEY
+            private_key: fromBase64(process.env.SHEET_PRIVATE_KEY)
         })
         await doc.loadInfo()
     
